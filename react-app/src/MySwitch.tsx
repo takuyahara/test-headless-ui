61230090ab196306186218dbe62fe06e6d3c5630
takuyahara <h.taku86@gmail.com>
2023-05-10 17:39:16 +0000

    Updated: added twind

--- react-app/src/MySwitch.tsx
+++ react-app/src/MySwitch.tsx
@@ -1,22 +1,23 @@
 import { useState } from 'react';
 import { Switch } from '@headlessui/react'
+import { tw } from 'twind'
 
 export default function MySwitch() {
   const [enabled, setEnabled] = useState(false)
 
   return (
     <Switch.Group>
-      <div className={`flex items-center`}>
-        <Switch.Label className={`mr-4`}>Enable notifications</Switch.Label>
+      <div className={tw`flex items-center`}>
+        <Switch.Label className={tw`mr-4`}>Enable notifications</Switch.Label>
         <Switch
           checked={enabled}
           onChange={setEnabled}
-          className={`${
+          className={tw`${
             enabled ? 'bg-blue-600' : 'bg-gray-200'
           } relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
         >
           <span
-            className={`${
+            className={tw`${
               enabled ? 'translate-x-6' : 'translate-x-1'
             } inline-block h-4 w-4 transform rounded-full bg-white transition-transform`}
           />
